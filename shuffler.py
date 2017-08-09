fbd1bf4bc9342a2bad60654ed936732ba5d458c8
jian13579 <jianhlu018@gmail.com>
2017-08-09 23:34:24 +0800

    first commit

---
+++ shuffler.py
@@ -0,0 +1,103 @@
+##This file will take in a deck list copy string and with the assistance of the hearthstims decklist reader,
+##return 3 or 4 cards based on the coin
+from hearthstone.deckstrings import Deck
+from hearthstone.enums import FormatType
+from matchingdbfid import *
+from matchingdbfidtoid import *
+import random
+import sys
+#import numpy
+
+def isFirst():
+    """
+    does a coin flip between two booleans, where true is first, and false is second
+    """
+    return random.choice([True, False])
+
+def opponent_selection(vector = ('Druid', 'Hunter', 'Mage', 'Paladin', 'Priest', 'Rogue', 'Shaman','Warlock', 'Warrior')):
+    """
+    takes in a vector of 9 different probabilities based on the percentages of each
+    class that appears in the meta currently. In the future we want to look for
+    distinguishments in the meta based on rank differences (i.e. legends, 1-5, and 5+)
+    Vector form should be in (druid, hunter, mage, paladin, priest, rogue, shaman, warlock, warrior)
+    in order to match the probabilities based on the VS data reaper live.
+    """
+    ## As we move on to include the classes and a full probability distribution,
+    ## the numpy.choice function will encompass all of that
+    ## we want to be able to pull probabilities from the google doc
+    return random.choice(vector)
+
+
+def fulldecklist(deckcode):
+    """
+    This function takes in the deckcode, and deciphers the deckcode into the complete list of cards
+    where the tuples (x,y) are separated into a list [x] y times.
+    """
+    ##First imports the deckcode as a deck
+    ##Example Mage Deck we will be using ("AAECAf0ECO0Fcem6AoivAuwHobcCxQS/CAuBsgKVA8HBApYF17YCmMQCqwTAAbC8ArsCo7YCAA==")
+
+    deck = Deck.from_deckstring(str(deckcode))
+    complete_deck = []
+    for card,copy in deck.cards:
+      l = [card] * copy
+      complete_deck.extend(l)
+    return complete_deck
+
+def initial_draw(decklist, isFirst):
+    """
+    takes in the code of the decklist, and then shuffles the decklist, and based on random we draw 3 or 4 cards(elements of the list)
+    """
+
+    ##First imports the deckcode as a deck
+    ##Example Mage Deck we will be using ("AAECAf0ECO0Fcem6AoivAuwHobcCxQS/CAuBsgKVA8HBApYF17YCmMQCqwTAAbC8ArsCo7YCAA==")
+    #deck = Deck.from_deckstring(str(deckcode))
+
+    ## from this deck we are able to create the hero, format, and decklist
+    ## we first generate the list of tuples which tell the amount, and the cardID for each card
+    ## In order for this program to be more efficient, we can implement the id to name only when the cards are drawn
+    ## and shown to the players
+    ## An example of the list of cards is a list of tuples showing (cardID, #of cards in deck)
+    ## [(749, 1), (113, 1), (40297, 1), (38792, 1), (1004, 1), (39841, 1), (581, 1),
+    ## (1087, 1), (39169, 2), (405, 2), (41153, 2), (662, 2), (39767, 2), (41496, 2), (555, 2),
+    ## (192, 2), (40496, 2), (315, 2), (39715, 2)]
+
+    ## Turns the decklist into a list that is shufflable
+    #decklist = fulldecklist(decklist)
+
+    ##randomizes the decklists and then returns the first 3 cards
+    #print(decklist)
+    random.shuffle(decklist)
+    #print(decklist)
+    if isFirst:
+      cardone = decklist.pop()
+      cardtwo = decklist.pop()
+      cardthree = decklist.pop()
+      #print(cardone, cardtwo, cardthree)
+      #cardone = matchdbfid(str(cardone))
+      #cardtwo = matchdbfid(str(cardtwo))
+      #cardthree = matchdbfid(str(cardthree))
+      #print(cardone, cardtwo, cardthree)
+      return [cardone, cardtwo, cardthree]
+
+    else:
+      cardone = decklist.pop()
+      cardtwo = decklist.pop()
+      cardthree = decklist.pop()
+      cardfour = decklist.pop()
+      #print(cardone, cardtwo, cardthree, cardfour)
+      #cardone = matchdbfid(str(cardone))
+      #cardtwo = matchdbfid(str(cardtwo))
+      #cardthree = matchdbfid(str(cardthree))
+      #cardfour = matchdbfid(str(cardfour))
+      #print(cardone, cardtwo, cardthree, cardfour)
+      return [cardone, cardtwo, cardthree, cardfour]
+
+def final_draw(decklist, number_tossed):
+  """
+  Takes the decklist and draws the number of cards mulliganed away from it
+  """
+  new_cards = []
+  while number_tossed > 0:
+    new_cards.append(decklist.pop())
+    number_tossed -= 1
+  return new_cards
---
+++ simulator.py
@@ -0,0 +1,93 @@
+from shuffler import *
+from hearthstone.deckstrings import Deck
+from hearthstone.enums import FormatType
+
+
+
+class simulator:
+
+    def __init__(self):
+        """
+        Information to be written in the database
+        """
+        self.decklist = []
+        self.order = None
+        self.originaldecklist = None
+        self.deck = None
+        self.opponent = ""
+        self.hand = []
+        self.deckcode = None
+        self.user_selection = None
+        self.urls = []
+
+    def simulate_mulligan(self, deckcode):
+        #determine order & draw initial cards
+        self.deckcode = str(deckcode)
+        self.order = isFirst()
+        self.deck = Deck.from_deckstring(self.deckcode)
+        self.decklist = fulldecklist(self.deckcode)
+        self.originaldecklist = self.decklist
+        random.shuffle(self.decklist)
+        self.opponent = opponent_selection()
+        self.hand = initial_draw(self.decklist, self.order)
+        self.display_cards(self.hand)
+        self.display_cards_ID(self.hand)
+
+    def display_cards(self, cards):
+        """
+        matches the dbfids to the numbers outputted by the strings in everysingle card
+        """
+        to_display = []
+        for card in cards:
+            to_display.append(matchdbfid(str(card)))
+        print(to_display)
+
+    def display_cards_ID(self, cards):
+        """
+        matches the dbfids to the ids of the cards and then to the images
+        """
+        CardIDs = []
+        for card in cards:
+            CardIDs.append(matchdbfidtoid(str(card)))
+        print(CardIDs)
+        return CardIDs
+
+    def ID_to_url(self, cards):
+        """
+        matches the ids to the urls of the card images
+        """
+        Cardurls = []
+        for card in cards:
+            CardIDs.append(matchidtourl(str(card)))
+        return Cardurls
+        
+    def replace_cards(self):
+        """
+        Choosing an index to replace what ever card is at the index that one decides
+        """
+        self.user_selection = input("Which cards would you like to keep (0 for keep & 1 for toss, space separated):")
+        self.user_selection = self.user_selection.split()
+        trash = [position for position, choice in enumerate(self.user_selection) if choice == '1']
+        for hand_index in trash:
+            self.hand.insert(hand_index, self.decklist.pop())
+            del self.hand[hand_index + 1]
+        return self.hand
+"""
+    def reset(self):
+
+        choice to restart with same deck, enter a new deck, or exit the simulation.
+        we do not need to include an else, since we will turn this to click based
+        upon interface creation
+
+        choice = input("To simulate the same deck press 1, to simulate a new decklist press 2, to exit the simulation press 3:")
+        if choice == '1':
+            self.simulate_mulligan(self.deckcode)
+
+        if choice == '2':
+            newdeckcode = str(input("Enter the deckcode of the deck you wish to practice:"))
+            self.simulate_mulligan(newdeckcode)
+
+        if choice == '3':
+            print("Thanks for playing")
+            return False
+"""
